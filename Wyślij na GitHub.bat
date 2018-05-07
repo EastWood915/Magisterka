5ed6a0c349d00e5c6cce157379d49268589b6980
Wojtek <wojtas1993@gmail.com>
2018-05-07 23:50:57 +0200

    2018_05_07_23_50_56

--- Wyślij na GitHub.bat
+++ Wyślij na GitHub.bat
@@ -2,7 +2,7 @@
 set datetimef=%date:~-4%_%date:~3,2%_%date:~0,2%_%time:~0,2%_%time:~3,2%_%time:~6,2%
 
 git add .
-git commit -m %datetimef
+git commit -m %datetimef%
 git push origin master
 
 pause 
